4f7d36af8dbc88630cd6783d1125eac6df754703
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-21 12:51:16 +0530

    Some sizeof testing

---
+++ Introduction_to_cpp/find_all_prime_numbers_till_n.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define endl "\n"
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    bool yes = 0;
+    short int srt {};
+    long int lng {};
+    unsigned long int ul {};
+
+    cout << sizeof(srt) << endl;
+    cout << sizeof(lng) << endl;
+    cout << sizeof(ul) << endl;
+    // Write your code from here
+    // cout << (3)/float(2);
+    cout << boolalpha;
+    cout << yes;
+    cout << INT_MAX;
+    return 1;
+}
