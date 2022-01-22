36bbdfb5bc37d7e7248349c72d12b9e639d06f53
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-22 19:43:03 +0530

    variables constant keywords are completed

--- C_language_revision/variables_constant_keywords.c
+++ C_language_revision/variables_constant_keywords.c
@@ -3,8 +3,22 @@
 // using namespace std;
 
 int main(){
+    // float temp = 3.1435724723497234;
+    // int just = 34;
+    // printf("The size of temp is: %d\n", &just);
+    // printf("The value of pi is: %f", temp);
 
-    printf("Hey there");
+    //#########################################
+
+    // int a = 0 , b= 0;
+    // printf("Put the value of a: ");
+    // scanf("%d", &a);
+    // printf("Put the value of b: ");
+    // scanf("%d", &b);
+    // printf("The sum of a and b is: %d", a + b);
+
+    //#####################################
+
     
     return 0;
 }
