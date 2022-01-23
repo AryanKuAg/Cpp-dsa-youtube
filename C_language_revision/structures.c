434f8ba0e0561e97216a6def450ef92b47a31b2e
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-24 03:48:08 +0530

    learning string input and output in C language

---
+++ C_language_revision/structures.c
@@ -0,0 +1,71 @@
+// #include<stdio.h>
+// #include<string.h>
+// //strucutre can hold dissimilar data
+
+// struct Car{
+//     char name[20] ;
+//     float price;
+// };
+
+// void printArray(char * ptr){
+//     int counter = 0;
+//     while(ptr[counter] != '\0'){
+//         printf("%c", ptr[counter]);
+//         counter++;
+//     }
+// }
+
+// int main(){
+
+
+//     struct Car ferrari;
+//     strcpy(ferrari.name, "Lamborgini");
+//     ferrari.price = 500000.00;
+//     printf("The name of the ferrari is: ");
+//     printArray(ferrari.name);
+//     printf("\n");
+//     printf("The price of this thing is: %.2f", ferrari.price );
+
+
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// // printf and scanf tutorial
+// // master class complete understanding
+// // taking input array -> done
+// // printing out array -> done
+// //
+// int main()
+// {
+// // format specifier and string issue input and output
+//     int temp[3] = {0,100};
+
+//     int counter = 0;
+//     while(counter < 3){
+
+//         printf("Enter the value of %d element: ", counter + 1);
+//         scanf("%d", temp + counter);
+
+//         counter++;
+//     }
+
+//     int pconter = 0;
+//     while(pconter < 3){
+//         printf("%d ", temp[pconter]);
+//         pconter++;
+//     }
+    
+
+
+
+//     return 0;
+// }
+
+#include<stdio.h>
+
+// string input -> not done
+// string output -> not done
+// 
