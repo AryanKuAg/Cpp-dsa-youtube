eb1818e8b8f1a53351c4524009840bd3008d7a5e
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-23 19:19:23 +0530

    instruction and oprators completed

--- C_language_revision/instructions_and_operators.c
+++ C_language_revision/instructions_and_operators.c
@@ -1,9 +1,23 @@
 #include<stdio.h>
+#include<math.h>
+
+void just();
+// int op int -> int
+// int op float -> float
+// float or float -> float
 
 int main(){
     // float temp = 343.3532;
     // printf("%.2f",temp);
-    434 > 0 ? printf("yes") : printf("no");
-
+    // 434 > 0 ? printf("yes") : printf("no");
+    pow(3,5);
+    just();
+    ///////////////
+    printf("\n%f", 40.5/2);
     return 0;
 }
+
+void just(){
+    printf("The function");
+}
+
