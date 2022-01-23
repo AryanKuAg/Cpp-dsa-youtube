3aaab3bd0d4d6b4bc30878157afbbab20f83818e
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-23 20:03:03 +0530

    loop control instructions done break continue;

---
+++ C_language_revision/loop_control_instructions.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    // for (size_t i = 0 ; i < 10; ++i){
+    //     printf("%d\n", i);
+    // }
+    // int i = 0;
+    // while (i < 10)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
+    // int j = 1000;
+    // do {
+    //     printf("%d\n", j);
+    //     j++;
+    // }while(j < 10);
+
+    for (size_t i = 0; printf("%d\n",i) ; printf("ARyan naam hai mera",i)){
+        
+       
+        if (i == 1){
+            break;
+        }i++;
+        if ( i == 30){
+            continue;
+        }
+    }
+    
+}
