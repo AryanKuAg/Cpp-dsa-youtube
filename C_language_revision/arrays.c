b120ebebcc922fb0c7b22eb9f4505268ba2d8853
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-23 21:33:16 +0530

    arrays done

---
+++ C_language_revision/arrays.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+// 2d array -> Array[0][0]
+
+int main(){
+    // int temp[50] = {34,5,35,2,3,43,5,2};
+
+    // for(size_t i = 0 ; i < sizeof(temp)/ sizeof(int);i++){
+    //     printf("%d\n", *(temp + i));
+    // }
+
+    // char temp[100] = "Aryan agrawal";
+    // int counter = 0 ;
+    // while(temp[counter] != '\0'){
+    //     printf("%c", temp[counter]);
+    //     counter++;
+    // }
+
+    return 0;
+}
