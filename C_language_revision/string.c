62921b91aa09203d055689f1a42e335d471c1f3b
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-23 22:01:41 +0530

    String added

---
+++ C_language_revision/string.c
@@ -0,0 +1,44 @@
+// #include<stdio.h>
+// void print_me(char* ptr);
+
+// int main(){
+//     char temp1[] = {'a','r','y','a','n','\0'};
+//     char temp2[] = "aryan";
+
+//     printf("is temp1 and temp2 are equal: %d\n",temp1 == temp2);
+//     print_me(temp1);
+//     printf("\n");
+//     print_me(temp2);
+
+//     // string name;
+// }
+
+// void print_me(char* ptr){
+//     int counter= 0;
+
+//     while(ptr[counter] != '\0'){
+//         printf("%c", ptr[counter]);
+//         counter++;
+//     }
+// }
+
+#include<stdio.h>
+#include<string.h>
+
+// int main(){
+//     char * just = "aryan is the boy";
+//     gets(just);
+//     puts(just);
+//     // gets();
+//     return 0;
+// }
+
+// int main(){
+//     char name[] = "there are people";
+//     // char name[] =  "Myname is aran hai";
+//     gets(name);
+//     char another[50] = {'0'};
+//     strcpy(another, name) ;
+
+//     puts(another);
+}
