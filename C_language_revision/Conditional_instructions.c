2003f58b8a0859c7ecd2a9b32bcd125f3b1b18bd
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-23 19:43:23 +0530

    learned switch and conditional checking

---
+++ C_language_revision/Conditional_instructions.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+// work in global scope 
+// don't define nested variables and call it in global scope idiot!!!
+
+int main(){
+    
+    // printf("is 3 == 3: %d", 3<=3);
+    // if (5 == 5 && 4 ==40 ){
+    //     printf("Cool bro cool");
+    // } else if (5 ==50 && 34 !=35){
+    //     printf("Cool again bro cool again");
+    // }
+    // else{
+    //    int c = 43;
+    // }
+    // printf("\n%d",c);
+
+    ///////////SWITCH-CASE/////////////////
+    // int temp = 44;
+
+    // switch(temp){
+    //     case 432:
+    //         printf("just a name");
+    //         break;
+    //     case 44:
+    //         printf("A Match Found");
+    //         break;
+    //     default:
+    //         printf("Nothing found");
+    // };
+    return 0;
+}
