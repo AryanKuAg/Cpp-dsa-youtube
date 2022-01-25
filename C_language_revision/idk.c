16e7745d4d9a5de6d7bc373e5899273547022040
Aryan Agrawal <alemantrixaryan@gmail.com>
2022-01-25 21:12:36 +0530

    All I did was this today

---
+++ C_language_revision/idk.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define ARYAn 50
+
+int main(){
+    // _Bool the = 3==3;
+    // printf("%d", the);
+    
+    // printf("%d", ARYAn);
+    // char temp[40] = {'a','b','*', [9] = 's'};
+    // printf("%c",temp[9]);
+    int array[2300];
+    printf("%d",array[302]);
+    
+}
